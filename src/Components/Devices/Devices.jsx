2094927f684ffb97a02b3095ee8e32bbe6462169
Practice <ahmed15-5729@diu.edu.bd>
2025-09-22 12:33:12 +0600

    3

---
+++ src/Components/Devices/Devices.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Device = () => {
+    return (
+        <div>
+                <h2>This is device</h2>
+        </div>
+    );
+};
+
+export default Device;
--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -1,17 +1,133 @@
 import React from "react";
+import Devices from "../Devices/Devices";
 
 const Home = () => {
   return (
-    <div className="">
-      <section className="bg-[#9538E2] px-12">
+    <div>
+    
+      <section className="bg-[#9538E2] px-12 flex items-center justify-center flex-col gap-4 pb-60 rounded-b-2xl overflow-hidden">
+        <h1 className="text-5xl font-bold text-center text-white">
+          Upgrade Your Tech Accessories with <br /> Gadget Heaven Accessories
+        </h1>
+        <p className="text-sm text-center text-white">
+          Explore the latest gadgets that will take your experience to the next
+          level. From smart devices to <br /> the coolest accessories, we have
+          it all!
+        </p>
+        <button className="btn rounded-2xl bg-white border-0 text-[#9538E2]">
+          Shop Now
+        </button>
+      </section>
 
-<h1>
-Upgrade Your Tech Accessorize with Gadget Heaven Accessories
-</h1>
-<p>
-Explore the latest gadgets that will take your experience to the next level. From smart devices to the coolest accessories, we have it all!
-</p>
-<button>Shop Now</button>
+      <section className="flex justify-center -mt-48 w-[80%] max-w-[900px] mx-auto relative">
+        <div className="relative w-full rounded-3xl overflow-hidden border-[6px] border-white/20 backdrop-blur-sm">
+          <img
+            className="w-full object-cover rounded-3xl"
+            src="/assets/banner.jpg"
+            alt="Banner"
+          />
+        </div>
+      </section>
+<h1 className="text-3xl text-center mt-24 mb-12 font-bold">Explore Cutting-Edge Gadgets</h1>
+      <section className=" px-32">
+        <div className="heromin-h-screen">
+          <div className="hero-content flex-col lg:flex-row">
+            <div className="grid grid-cols-1 gap-4 bg-white py-6 px-4 rounded-2xl">
+              <button
+                className="
+  rounded-2xl
+  border-0
+  bg-gray-100 text-black
+  hover:bg-[#9538E2] hover:text-white
+  transition-colors duration-300
+  px-4 py-2
+"
+              >
+                All Products
+              </button>
+              <button
+                className="
+  rounded-2xl
+  border-0
+  bg-gray-100 text-black
+  hover:bg-[#9538E2] hover:text-white
+  transition-colors duration-300
+  px-4 py-2
+"
+              >
+                Laptops
+              </button>
+              <button
+                className="
+  rounded-2xl
+  border-0
+  bg-gray-100 text-black
+  hover:bg-[#9538E2] hover:text-white
+  transition-colors duration-300
+  px-4 py-2
+"
+              >
+                Phones
+              </button>
+              <button
+                className="
+  rounded-2xl
+  border-0
+  bg-gray-100 text-black
+  hover:bg-[#9538E2] hover:text-white
+  transition-colors duration-300
+  px-4 py-2
+"
+              >
+                Accessories
+              </button>
+              <button
+                className="
+  rounded-2xl
+  border-0
+  bg-gray-100 text-black
+  hover:bg-[#9538E2] hover:text-white
+  transition-colors duration-300
+  px-4 py-2
+"
+              >
+                Smart Watches
+              </button>
+              <button
+                className="
+  rounded-2xl
+  border-0
+  bg-gray-100 text-black
+  hover:bg-[#9538E2] hover:text-white
+  transition-colors duration-300
+  px-4 py-2
+"
+              >
+                MacBook
+              </button>
+              <button
+                className="
+  rounded-2xl
+  border-0
+  bg-gray-100 text-black
+  hover:bg-[#9538E2] hover:text-white
+  transition-colors duration-300
+  px-4 py-2
+"
+              >
+                Iphone
+              </button>
+            </div>
+
+            {/* //details section */}
+         <div>
+
+<Devices></Devices>
+
+
+         </div>
+          </div>
+        </div>
       </section>
     </div>
   );
